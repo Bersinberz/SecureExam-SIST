5c5bfbd5e320e749361d1a1dc5cfbaa86852cad0
Bersin <157630068+Bersinberz@users.noreply.github.com>
2025-01-18 19:23:03 +0530

    Delete SecureExam-SIST directory

--- SecureExam-SIST/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  enableKioskMode: () => ipcRenderer.send('enable-kiosk-mode'),
-  quitApp: () => ipcRenderer.send('quit-app'),
-  //startTimer: () => ipcRenderer.send('start-timer'),
-  //onTimerFinish: (callback) => ipcRenderer.on('timer-finish', callback),
-  openCompilerWindow: () => ipcRenderer.send('open-compiler-window'), // Add this line
-});
