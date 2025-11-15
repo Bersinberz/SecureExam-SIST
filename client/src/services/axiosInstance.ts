a2cb727ee80db4369dc61e16ee5a5f986c3d62e5
Bersin <bersinmail@gmail.com>
2025-11-15 14:21:54 +0530

    everything updated next is build docker nginx for production

--- client/src/services/axiosInstance.ts
+++ client/src/services/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios, {
 } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "process.env.API_BASE_URL",
   headers: {
     "Content-Type": "application/json",
   },
